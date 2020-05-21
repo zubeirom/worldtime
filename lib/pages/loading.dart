658154aa9733263ac6a4e5da0d01fe5ed881b1ad
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-05-22 00:57:58 +0200

    loading spinner

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:worldtime/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -11,13 +12,14 @@ class _LoadingState extends State<Loading> {
   String time = 'loading';
 
   void setupWorldTime() async {
-    WorldTime wt = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    WorldTime wt = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await wt.getTime();
-    Navigator.pushReplacementNamed(context, '/home', arguments: {
-      'location': wt.location,
-      'flag': wt.flag,
-      'time': wt.time
-    });
+//    Navigator.pushReplacementNamed(context, '/home', arguments: {
+//      'location': wt.location,
+//      'flag': wt.flag,
+//      'time': wt.time
+//    });
   }
 
   @override
@@ -29,10 +31,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text('loading'),
-      ),
+      backgroundColor: Colors.blue[900],
+        body: Center(
+          child: SpinKitDoubleBounce(
+            color: Colors.white,
+            size: 50.0,
+          ),
+        )
     );
   }
 }
